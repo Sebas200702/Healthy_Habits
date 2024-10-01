686ac14503d7252ba09feba8f0dab3cdc3a656d6
Sebas200702 <sebastorregroza6@gmail.com>
2024-10-01 05:33:08 -0500

    add supabase for database

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "auth-astro";
 import Google from "@auth/core/providers/google";
+import { supabase } from "./src/lib/supabase";
 
 export default defineConfig({
   providers: [
@@ -13,4 +14,28 @@ export default defineConfig({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const result = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("email", user.email);
+
+      if (result.data[0].email === user.email) {
+        return true;
+      } else {
+        const { data, error } = await supabase.from("profiles").upsert({
+          email: user.email,
+          user_name: user.name,
+          name: user.name,
+          avatar_url: user.image,
+        });
+        if (error) {
+          console.log(error);
+          return false;
+        }
+        return true;
+      }
+    },
+  },
 });
