82a95aa0da553fe08d61ceb3ef45c18f40c6a472
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2025-02-28 17:37:54 -0500

    Update gemini.ts

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = import.meta.env.GEMINI_API_KEY;
 const genAI: GoogleGenerativeAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemma-2-27b-it" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
 export const chat = model.startChat();
